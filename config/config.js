97ae5485ac2524cca5cfc10e31ce8b29dfd5154c
Miguchi-2609 <engoffin06@gmail.com>
2022-12-28 19:48:04 +0100

    c

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-var g = 'URL'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
-var h = 'PORT'; // PORT - Radionomy : 80
+var g = 'link.zeno.fm'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = '80'; // PORT - Radionomy : 80
 var z = ''; //Shoutcast='/;stream.mp3'
-var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
+var i = '/1tklzl98qvkvv'; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
 var l = ''; // Radionomy GUID (For Current Listen)
